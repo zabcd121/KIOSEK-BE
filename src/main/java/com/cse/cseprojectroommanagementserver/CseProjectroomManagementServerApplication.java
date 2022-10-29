6089b4575a4efe367d0246546e8f6d3c358ac868
zabcd121 <zabcd121@naver.com>
2022-10-29 16:01:19 +0900

    :tada: start CSEProjectRoomManagementServerApplication project!!

---
+++ src/main/java/com/cse/cseprojectroommanagementserver/CseProjectroomManagementServerApplication.java
@@ -0,0 +1,13 @@
+package com.cse.cseprojectroommanagementserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CseProjectroomManagementServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CseProjectroomManagementServerApplication.class, args);
+    }
+
+}
