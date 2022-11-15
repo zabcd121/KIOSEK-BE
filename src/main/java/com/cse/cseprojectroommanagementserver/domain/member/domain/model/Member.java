21479129ed6c8ab9c6f7ed071ff7d55029cc7016
zabcd121 <zabcd121@naver.com>
2022-11-15 12:49:28 +0900

    :package: model(update): stdNum 속성 삭제 및 Spring Security를 위한 메서드 추가
    로그인 아이디를 학번으로 사용하도록 변경돼서 stdNum 속성을 삭제하고 Security를 위한 권한 get메서드 추가

--- src/main/java/com/cse/cseprojectroommanagementserver/domain/member/domain/model/Member.java
+++ src/main/java/com/cse/cseprojectroommanagementserver/domain/member/domain/model/Member.java
@@ -1,13 +1,18 @@
 package com.cse.cseprojectroommanagementserver.domain.member.domain.model;
 
 import com.cse.cseprojectroommanagementserver.global.common.BaseEntity;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Member extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,7 +24,28 @@ public class Member extends BaseEntity {
     @Enumerated(value = EnumType.STRING)
     private RoleType roleType;
 
-    private String stdNum;
     private String name;
     private String email;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ACCOUNT_QR")
+    AccountQR accountQR;
+
+    public String getLoginId() {
+        return account.getLoginId();
+    }
+    public String getPassword() {
+        return account.getPassword();
+    }
+
+    public List<String> getAuthorities() {
+        List roleList = new ArrayList();
+
+        if(!roleType.equals("")) {
+            roleList.add(roleType.toString());
+        }
+
+        return roleList;
+    }
+
 }
