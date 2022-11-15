dea27da51d0c3842d32ffd0897e4c8db37ba530b
zabcd121 <zabcd121@naver.com>
2022-11-15 14:10:30 +0900

    :package: add: jwt token filter 추가

---
+++ src/main/java/com/cse/cseprojectroommanagementserver/global/jwt/JwtTokenFilter.java
@@ -0,0 +1,82 @@
+package com.cse.cseprojectroommanagementserver.global.jwt;
+
+import com.cse.cseprojectroommanagementserver.global.common.ResponseConditionCode;
+import com.cse.cseprojectroommanagementserver.global.jwt.exception.TokenNotBearerException;
+import com.cse.cseprojectroommanagementserver.global.jwt.exception.TokenNotPassedException;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static com.cse.cseprojectroommanagementserver.global.common.ResponseConditionCode.*;
+import static com.cse.cseprojectroommanagementserver.global.jwt.JwtTokenProvider.*;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private final RedisTemplate redisTemplate;
+
+    /**
+     * JWT를 검증하는 필터
+     * HttpServletRequest 의 Authorization 헤더에서 JWT token을 찾고 그것이 맞는지 확인
+     * UsernamePasswordAuthenticationFilter 앞에서 작동
+     * (JwtTokenFilterConfigurer 참고)
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("####doFilterInternal진입");
+        String jwt = resolveToken(request, AUTHORIZATION_HEADER);
+
+        try{
+            if ( jwt != null && jwtTokenProvider.validateToken(jwt)) {
+                String isLogout = (String) redisTemplate.opsForValue().get(jwt);
+
+                if(ObjectUtils.isEmpty(isLogout)) {
+                    Authentication authentication = jwtTokenProvider.getAuthentication(jwt);
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                    log.info("set Authentication to security context for '{}', uri: {}", authentication.getName(), request.getRequestURI());
+                }
+            }
+        } catch (SignatureException e) {
+            request.setAttribute("exception", e);
+        } catch (MalformedJwtException e) {
+            request.setAttribute("exception", e);
+        } catch (ExpiredJwtException e) {
+            request.setAttribute("exception", e);
+        } catch (UnsupportedJwtException e) {
+            request.setAttribute("exception", e);
+        } catch (JwtException | IllegalArgumentException e) {
+            request.setAttribute("exception", e);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request, String header) {
+        String bearerToken = request.getHeader(header);
+        if(bearerToken == null) {
+            throw new TokenNotPassedException(TOKEN_NOT_PASSED.getMessage());
+        } else if(!bearerToken.startsWith(BEARER)) {
+            throw new TokenNotBearerException(TOKEN_NOT_BEARER.getMessage());
+        }
+
+        return bearerToken.substring(7);
+    }
+}
