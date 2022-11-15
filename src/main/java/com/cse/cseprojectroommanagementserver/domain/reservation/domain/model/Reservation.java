95d73153e6669cc294f9eedac6307d3a9fde5ec1
zabcd121 <zabcd121@naver.com>
2022-11-15 13:54:01 +0900

    update(model): Join column naming Uppercase로 변경

--- src/main/java/com/cse/cseprojectroommanagementserver/domain/reservation/domain/model/Reservation.java
+++ src/main/java/com/cse/cseprojectroommanagementserver/domain/reservation/domain/model/Reservation.java
@@ -1,11 +1,10 @@
 package com.cse.cseprojectroommanagementserver.domain.reservation.domain.model;
 
 import com.cse.cseprojectroommanagementserver.domain.member.domain.model.Member;
-import com.cse.cseprojectroommanagementserver.domain.projectroom.domain.model.Table;
+import com.cse.cseprojectroommanagementserver.domain.projectroom.domain.model.Desk;
 import com.cse.cseprojectroommanagementserver.global.common.BaseEntity;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
@@ -20,12 +19,12 @@ public class Reservation extends BaseEntity {
     private Long reservationId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
+    @JoinColumn(name = "MEMBER_ID")
     private Member customer;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "table_id")
-    private Table table;
+    @JoinColumn(name = "DESK_ID")
+    private Desk desk;
 
     @Enumerated(value = EnumType.STRING)
     private ReservationStatus reservationStatus;
