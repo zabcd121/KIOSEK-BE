e2c205bc3cbd67e68ccc31db1ec2ced5fc74049c
zabcd121 <zabcd121@naver.com>
2022-11-15 13:55:11 +0900

    update(model): naming 수정

---
+++ src/main/java/com/cse/cseprojectroommanagementserver/domain/usebanLog/domain/model/UseBanLog.java
@@ -0,0 +1,23 @@
+package com.cse.cseprojectroommanagementserver.domain.usebanLog.domain.model;
+
+import com.cse.cseprojectroommanagementserver.domain.projectroom.domain.model.Desk;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UseBanLog implements Serializable {
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USEBANINFO_ID")
+    private UseBanInfo useBanInfo;
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "DESK_ID")
+    private Desk bannedDesk;
+}
