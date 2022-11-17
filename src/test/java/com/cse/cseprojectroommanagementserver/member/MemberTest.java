1a65754299f126e6a4e16965ad96dc486e219c10
zabcd121 <zabcd121@naver.com>
2022-11-17 23:30:55 +0900

    :white_check_mark: test(add): Member.createMember() 테스트 코드 작성

---
+++ src/test/java/com/cse/cseprojectroommanagementserver/member/MemberTest.java
@@ -0,0 +1,60 @@
+package com.cse.cseprojectroommanagementserver.member;
+
+import com.cse.cseprojectroommanagementserver.domain.member.domain.model.Account;
+import com.cse.cseprojectroommanagementserver.domain.member.domain.model.AccountQR;
+import com.cse.cseprojectroommanagementserver.domain.member.domain.model.Member;
+import com.cse.cseprojectroommanagementserver.domain.member.domain.model.RoleType;
+import com.cse.cseprojectroommanagementserver.global.common.Image;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberTest {
+
+    final String loginId = "20230101";
+    final String password = "example1234!";
+    final String name = "소공이";
+    final String email = "example@kumoh.ac.kr";
+
+    Member member;
+    Account account;
+    Image image;
+
+    @BeforeEach
+    void setUp() {
+        account = Account.builder().loginId(loginId).password(password).build();
+        image = Image.builder().fileLocalName("localName").fileOriName("account_qr").fileUrl("/Users/khs/Documents/images").content("randomContent").build();
+
+        member = Member.builder()
+                .account(account)
+                .name(name)
+                .email(email)
+                .roleType(RoleType.ROLE_MEMBER)
+                .accountQR(AccountQR.builder().qrCodeImg(image).build())
+                .build();
+
+        member.getAccountQR().setMember(member);
+    }
+
+    @DisplayName("Member.createMember() Member instance가 정상적으로 생성되는지와 AccountQR과의 양방향 관계 메서드가 제대로 작동했는지 확인한다.")
+    @Test
+    void createMember() {
+
+        //when
+        Member createdMember = Member.createMember(account, email, name, image);
+
+        //then
+        assertEquals(member.getAccountQR().getMember().getAccount(), createdMember.getAccountQR().getMember().getAccount());
+
+        assertEquals(member.getAccount(), createdMember.getAccount());
+        assertEquals(member.getEmail(), createdMember.getEmail());
+        assertEquals(member.getName(), createdMember.getName());
+        assertEquals(member.getAccountQR().getQrCodeImg(), createdMember.getAccountQR().getQrCodeImg());
+        assertEquals(member.getRoleType(), createdMember.getRoleType());
+
+    }
+}
