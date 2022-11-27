8390c5318470eb9b00a0e349449efe07a09f9a5f
zabcd121 <zabcd121@naver.com>
2022-11-28 00:36:25 +0900

    :package: ex(add): 예약 관련 발생 Runtime 예외 추가

---
+++ src/main/java/com/cse/cseprojectroommanagementserver/domain/reservation/exception/InvalidReservationStatusCodeException.java
@@ -0,0 +1,5 @@
+package com.cse.cseprojectroommanagementserver.domain.reservation.exception;
+
+public class InvalidReservationStatusCodeException extends RuntimeException{
+    public InvalidReservationStatusCodeException(){}
+}
