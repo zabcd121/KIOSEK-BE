06a93f134ff984d36befb16ddb6f25b785525205
zabcd121 <zabcd121@naver.com>
2022-11-28 00:22:36 +0900

    :sparkles: feat: 예약 생성(일반, 현장) 생성 메서드 추가

--- src/main/java/com/cse/cseprojectroommanagementserver/domain/reservation/domain/model/Reservation.java
+++ src/main/java/com/cse/cseprojectroommanagementserver/domain/reservation/domain/model/Reservation.java
@@ -1,37 +1,99 @@
 package com.cse.cseprojectroommanagementserver.domain.reservation.domain.model;
 
 import com.cse.cseprojectroommanagementserver.domain.member.domain.model.Member;
-import com.cse.cseprojectroommanagementserver.domain.projectroom.domain.model.Desk;
-import com.cse.cseprojectroommanagementserver.global.common.BaseEntity;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+import com.cse.cseprojectroommanagementserver.domain.projecttable.domain.model.ProjectTable;
+import com.cse.cseprojectroommanagementserver.domain.tablereturn.domain.model.TableReturn;
+import com.cse.cseprojectroommanagementserver.global.common.BaseTimeEntity;
+import com.cse.cseprojectroommanagementserver.global.common.Image;
+import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
+import static com.cse.cseprojectroommanagementserver.domain.reservation.domain.model.Means.*;
+import static com.cse.cseprojectroommanagementserver.domain.reservation.domain.model.ReservationStatus.*;
+import static com.cse.cseprojectroommanagementserver.global.util.DateFormatProvider.*;
+
 @Entity
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 @EntityListeners(AuditingEntityListener.class)
-public class Reservation extends BaseEntity {
+public class Reservation extends BaseTimeEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long reservationId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "MEMBER_ID")
-    private Member customer;
+    @JoinColumn(name = "member_id")
+    private Member member;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "DESK_ID")
-    private Desk desk;
+    @JoinColumn(name = "project_table_id")
+    private ProjectTable projectTable;
+
+    @OneToOne(mappedBy = "targetReservation", fetch = FetchType.LAZY)
+    private TableReturn tableReturn;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "reservation_qr_id")
+    private ReservationQR reservationQR;
 
     @Enumerated(value = EnumType.STRING)
     private ReservationStatus reservationStatus;
 
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
+    @DateTimeFormat(pattern = LOCAL_DATE_TIME_FORMAT)
+    private LocalDateTime startDateTime;
 
+    @DateTimeFormat(pattern = LOCAL_DATE_TIME_FORMAT)
+    private LocalDateTime endDateTime;
+
+    @DateTimeFormat(pattern = LOCAL_DATE_TIME_FORMAT)
     private LocalDateTime checkInTime;
+
+    @Enumerated(EnumType.STRING)
+    private Means means;
+
+    public void changeReservationQR(ReservationQR reservationQR) {
+        if(this.reservationQR != null) {
+            this.reservationQR.changeReservation(null);
+        }
+        this.reservationQR = reservationQR;
+        this.reservationQR.changeReservation(this);
+    }
+
+    public static Reservation createReservation(Member member, ProjectTable projectTable, Image qrImage, LocalDateTime startDateTime, LocalDateTime endDateTime) {
+        Reservation reservation = Reservation.builder()
+                .member(member)
+                .projectTable(projectTable)
+                .reservationStatus(RESERVATION_COMPLETED)
+                .startDateTime(startDateTime)
+                .endDateTime(endDateTime)
+                .means(WEB)
+                .build();
+
+        reservation.changeReservationQR(ReservationQR.builder().image(qrImage).build());
+
+        return reservation;
+    }
+
+    public static Reservation createOnSiteReservation(Member member, ProjectTable projectTable, LocalDateTime startDateTime, LocalDateTime endDateTime) {
+        return Reservation.builder()
+                .member(member)
+                .projectTable(projectTable)
+                .reservationStatus(IN_USE)
+                .startDateTime(startDateTime)
+                .endDateTime(endDateTime)
+                .means(KIOSK)
+                .build();
+    }
+
+    public void changeStatusToReturned(TableReturn tableReturn) {
+        this.tableReturn = tableReturn;
+        this.reservationStatus = RETURNED;
+    }
 
 }
