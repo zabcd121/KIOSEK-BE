1602e225e73c1671571455758e7f54ddf01eb81f
zabcd121 <zabcd121@naver.com>
2022-11-28 00:12:58 +0900

    :recycle: refactor: 의존 repo 네이밍 변경 전파

--- src/main/java/com/cse/cseprojectroommanagementserver/domain/penalty/application/PenaltyService.java
+++ src/main/java/com/cse/cseprojectroommanagementserver/domain/penalty/application/PenaltyService.java
@@ -1,19 +1,15 @@
 package com.cse.cseprojectroommanagementserver.domain.penalty.application;
 
-import com.cse.cseprojectroommanagementserver.domain.member.domain.model.Member;
-import com.cse.cseprojectroommanagementserver.domain.penalty.domain.repository.PenaltyUserCheckRepository;
-import com.cse.cseprojectroommanagementserver.domain.reservation.exception.PenaltyMemberReserveFailException;
+import com.cse.cseprojectroommanagementserver.domain.penalty.domain.repository.PenaltySearchableRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import static com.cse.cseprojectroommanagementserver.global.common.ResponseConditionCode.RESERVATION_FAIL_PENALTY_USER;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class PenaltyService {
 
-    private final PenaltyUserCheckRepository penaltyUserRepository;
-    
+    private final PenaltySearchableRepository penaltyUserRepository;
+
 }
