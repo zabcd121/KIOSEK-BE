bc259b2431a081a8552078423c2ee8fbd9ccfba1
zabcd121 <zabcd121@naver.com>
2022-11-28 00:05:47 +0900

    :recycle: refactor: 코드라인 변경 및 BaseTimeEntity네이밍 변경 전파

--- src/main/java/com/cse/cseprojectroommanagementserver/domain/penalty/domain/model/Penalty.java
+++ src/main/java/com/cse/cseprojectroommanagementserver/domain/penalty/domain/model/Penalty.java
@@ -1,9 +1,9 @@
 package com.cse.cseprojectroommanagementserver.domain.penalty.domain.model;
 
+import com.cse.cseprojectroommanagementserver.domain.member.domain.model.Member;
 import com.cse.cseprojectroommanagementserver.domain.violation.domain.model.Violation;
-import com.cse.cseprojectroommanagementserver.global.common.BaseEntity;
+import com.cse.cseprojectroommanagementserver.global.common.BaseTimeEntity;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
@@ -12,16 +12,19 @@ import java.util.List;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Penalty extends BaseEntity {
+public class Penalty extends BaseTimeEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long penaltyId;
 
-    private String description;
-
-    @OneToMany(fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "penalty", fetch = FetchType.LAZY)
     private List<Violation> violations;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    private String description;
     private LocalDate startDate;
     private LocalDate endDate;
 
