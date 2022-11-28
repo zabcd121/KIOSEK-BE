d0ae7d60dabf18d3a3d2af3b10d7079f27a3067a
zabcd121 <zabcd121@naver.com>
2022-11-28 11:14:32 +0900

    :package: model(update): Penalty Entity와 N:1 관계 추가
    Violation을 Penalty와의 연관계에서 주인으로 설정

--- src/main/java/com/cse/cseprojectroommanagementserver/domain/violation/domain/model/Violation.java
+++ src/main/java/com/cse/cseprojectroommanagementserver/domain/violation/domain/model/Violation.java
@@ -1,6 +1,7 @@
 package com.cse.cseprojectroommanagementserver.domain.violation.domain.model;
 
 import com.cse.cseprojectroommanagementserver.domain.member.domain.model.Member;
+import com.cse.cseprojectroommanagementserver.domain.penalty.domain.model.Penalty;
 import com.cse.cseprojectroommanagementserver.domain.reservation.domain.model.Reservation;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -28,6 +29,8 @@ public class Violation {
     @JoinColumn(name = "reservation_id")
     private Reservation targetReservation;
 
-
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "penalty_id")
+    private Penalty penalty;
 
 }
