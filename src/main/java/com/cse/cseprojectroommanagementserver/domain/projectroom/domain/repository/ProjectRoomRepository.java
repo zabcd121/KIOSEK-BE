a124b420c9c158720e139060b4b8e48f1e82bb1e
zabcd121 <zabcd121@naver.com>
2022-12-02 10:49:34 +0900

    :package: repo(add): ProjectRoom 모델 기본 repository 정의

---
+++ src/main/java/com/cse/cseprojectroommanagementserver/domain/projectroom/domain/repository/ProjectRoomRepository.java
@@ -0,0 +1,12 @@
+package com.cse.cseprojectroommanagementserver.domain.projectroom.domain.repository;
+
+import com.cse.cseprojectroommanagementserver.domain.projectroom.domain.model.ProjectRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProjectRoomRepository extends JpaRepository<ProjectRoom, Long> {
+}
