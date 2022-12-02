d22820333194fcd38ae68d3b93217b976d99be0d
zabcd121 <zabcd121@naver.com>
2022-12-02 09:39:37 +0900

    :wrench: config(add): @EnableScheduling 설정
    스케줄링 사용 위해서 추가

--- src/main/java/com/cse/cseprojectroommanagementserver/CseProjectRoomManagementServerApplication.java
+++ src/main/java/com/cse/cseprojectroommanagementserver/CseProjectRoomManagementServerApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.persistence.EntityManager;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class CseProjectRoomManagementServerApplication {
 
     public static void main(String[] args) {
