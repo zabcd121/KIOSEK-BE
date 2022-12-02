ba3f5923a66ccc4af2d95d4d44805764c565b5d6
zabcd121 <zabcd121@naver.com>
2022-12-02 09:37:25 +0900

    :construction_worker: build: dockerfile image build

--- src/main/java/com/cse/cseprojectroommanagementserver/global/config/FileConfig.java
+++ src/main/java/com/cse/cseprojectroommanagementserver/global/config/FileConfig.java
@@ -1,10 +1,12 @@
 package com.cse.cseprojectroommanagementserver.global.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Slf4j
 @Configuration
 public class FileConfig implements WebMvcConfigurer {
 
@@ -13,16 +15,21 @@ public class FileConfig implements WebMvcConfigurer {
 
     @Value("${fileDir.outbound}")
     private String outBoundDir;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("file config 진입");
         /**
          * images/account/2022/01/01/qr.img
          * images/reservation/2022/01/01/qr.img
          * images/complaint/2022/01/01/qr.img
          * images/** 로 들어오면 -> Ueers/doucments/qrCode/images/** 로 매핑 해주도록 하면 됨
          */
+//        registry
+//                .addResourceHandler("/images/**")
+//                .addResourceLocations("file:///Users/khs/Documents/images/");
         registry
-                .addResourceHandler(inBoundDir)
-                .addResourceLocations("file://:"+outBoundDir);
+                .addResourceHandler(inBoundDir + "/**")
+                .addResourceLocations("file://" + outBoundDir + "/");
     }
 }
