8049e0804864e79d7872d431a3450c69773f936b
zabcd121 <zabcd121@naver.com>
2022-12-02 10:59:14 +0900

    :sparkles: feat: 예약 관련 api 추가..
    1. 현장에서 계정 QR 인증 후 예약 Post api
    2. 현장에서 계정 폼 로그인을 통해 인증 후 예약 Post api
    3. QA스피커에서 현재 이 테이블이 예약된 테이블인지 Get api
    4. 현재 멤버 자신의 예약된 리스트 Get api
    5. 멤버 자신의 과거 예약 리스트 Get api
    6. 예약 취소 patch api
    7. 예약 QR인증 patch api

--- src/main/java/com/cse/cseprojectroommanagementserver/domain/reservation/api/ReservationApiController.java
+++ src/main/java/com/cse/cseprojectroommanagementserver/domain/reservation/api/ReservationApiController.java
@@ -1,8 +1,12 @@
 package com.cse.cseprojectroommanagementserver.domain.reservation.api;
 
+import com.cse.cseprojectroommanagementserver.domain.member.application.AuthService;
+import com.cse.cseprojectroommanagementserver.domain.member.domain.model.Member;
+import com.cse.cseprojectroommanagementserver.domain.reservation.application.ReservationAuthService;
+import com.cse.cseprojectroommanagementserver.domain.reservation.application.ReservationStatusChangeService;
 import com.cse.cseprojectroommanagementserver.domain.reservation.application.ReserveTableService;
 //import com.cse.cseprojectroommanagementserver.domain.reservation.dto.ReservationSearchDto;
-import com.cse.cseprojectroommanagementserver.domain.reservation.application.SearchReservationService;
+import com.cse.cseprojectroommanagementserver.domain.reservation.application.ReservationSearchService;
 import com.cse.cseprojectroommanagementserver.global.common.dto.ResponseSuccess;
 import com.cse.cseprojectroommanagementserver.global.common.dto.ResponseSuccessNoResult;
 import lombok.RequiredArgsConstructor;
@@ -20,7 +24,10 @@ import static com.cse.cseprojectroommanagementserver.global.common.ResponseCondi
 public class ReservationApiController {
 
     private final ReserveTableService reserveTableService;
-    private final SearchReservationService searchReservationService;
+    private final ReservationSearchService reservationSearchService;
+    private final ReservationStatusChangeService reservationStatusChangeService;
+    private final ReservationAuthService reservationAuthService;
+    private final AuthService authService;
 
     //일반 웹 예약
     @PostMapping
@@ -30,29 +37,59 @@ public class ReservationApiController {
     }
 
     //현장 예약
-    @PostMapping("/onsite")
-    public ResponseSuccessNoResult reserveOnSite(@RequestBody ReserveRequest reserveRequest) {
-        reserveTableService.reserveOnsite(reserveRequest);
+    @PostMapping("/onsite/qr")
+    public ResponseSuccessNoResult reserveOnSiteByQRLogin(@RequestBody OnsiteReservationRequestByQR reservationRequest) {
+        Member matchedMember = authService.getMatchedMember(reservationRequest.getAccountQRContents());
+        reserveTableService.reserveOnsiteByAccountQR(matchedMember, reservationRequest);
         return new ResponseSuccessNoResult(RESERVATION_SUCCESS);
     }
 
-    @GetMapping("/projectroom/{id}")
+    @PostMapping("/onsite/form")
+    public ResponseSuccessNoResult reserveOnSiteByFromLogin(@RequestBody OnsiteReservationRequestByLoginForm reservationRequest) {
+        Member matchedMember = authService.getMatchedMember(reservationRequest.getLoginId(), reservationRequest.getPassword());
+        reserveTableService.reserveOnsiteByFormLogin(matchedMember, reservationRequest);
+
+        return new ResponseSuccessNoResult(RESERVATION_SUCCESS);
+    }
+
+    @GetMapping("/projectrooms/{id}")
     public ResponseSuccess<List<SearchReservationResponse>> getReservationListByProjectRoom(@PathVariable("id") Long projectRoomId,
-                                                                                         @RequestParam FirstAndLastDateTimeRequest firstAndLastDateTimeRequest) {
-        List<SearchReservationResponse> searchReservationList = searchReservationService.searchReservationListByProjectRoom(projectRoomId, firstAndLastDateTimeRequest);
+                                                                                         @ModelAttribute FirstAndLastDateTimeRequest firstAndLastDateTimeRequest) {
+        List<SearchReservationResponse> searchReservationList = reservationSearchService.searchReservationListByProjectRoom(projectRoomId, firstAndLastDateTimeRequest);
         return new ResponseSuccess(RESERVATION_SEARCH_SUCCESS, searchReservationList);
     }
 
+    @GetMapping("/tables")
+    public ResponseSuccessNoResult isCurrentReservedTable(@RequestParam String tableName) {
+        reservationSearchService.checkIsInUseTableAtCurrent(tableName);
+        return new ResponseSuccessNoResult(IN_USE_TABLE);
+    }
+
     @GetMapping("/current/members/{id}")
-    public ResponseSuccess<List<CurrentReservationByMemberResponse>> getMemberCurrentReservationList(@PathVariable("id") Long memberId) {
-        List<CurrentReservationByMemberResponse> currentReservationList = searchReservationService.searchCurrentReservationListByMember(memberId);
-        return new ResponseSuccess(RESERVATION_SEARCH_SUCCESS, currentReservationList);
+    public ResponseSuccess<List<CurrentReservationByMemberResponse>> getCurrentReservationListOfMember(@PathVariable("id") Long memberId) {
+        List<CurrentReservationByMemberResponse> myCurrentReservationList = reservationSearchService.searchMyCurrentReservationList(memberId);
+        return new ResponseSuccess(RESERVATION_SEARCH_SUCCESS, myCurrentReservationList);
+    }
+
+    @GetMapping("/past/members/{id}")
+    public ResponseSuccess<List<PastReservationByMemberResponse>> getPastReservationListOfMember(@PathVariable("id") Long memberId) {
+        List<PastReservationByMemberResponse> myPastReservationList = reservationSearchService.searchMyPastReservationList(memberId);
+        return new ResponseSuccess(RESERVATION_SEARCH_SUCCESS, myPastReservationList);
     }
 
     @PatchMapping("/{id}")
-    public void cancelReservation(@PathVariable("id") Long reservationId) {
-
+    public ResponseSuccessNoResult cancelReservation(@PathVariable("id") Long reservationId) {
+        reservationStatusChangeService.cancelReservation(reservationId);
+        return new ResponseSuccessNoResult(RESERVATION_CANCEL_SUCCESS);
     }
+
+    @PatchMapping("/auth/qr")
+    public ResponseSuccessNoResult authReservationQR(@RequestBody QRAuthRequest qrContents) {
+        reservationAuthService.qrReservationAuth(qrContents);
+        return new ResponseSuccessNoResult(RESERVATION_QR_AUTH_SUCCESS);
+    }
+
+
 
 
 
