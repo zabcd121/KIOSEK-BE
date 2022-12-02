2be7a029be1d437de6cf497ad13299a078fbe808
zabcd121 <zabcd121@naver.com>
2022-12-02 11:19:36 +0900

    :package: ex(add): 예약 관련 Runtime 예외 정의

---
+++ src/main/java/com/cse/cseprojectroommanagementserver/domain/reservation/exception/ImpossibleCheckInTImeException.java
@@ -0,0 +1,5 @@
+package com.cse.cseprojectroommanagementserver.domain.reservation.exception;
+
+public class ImpossibleCheckInTImeException extends RuntimeException{
+    public ImpossibleCheckInTImeException() {}
+}
