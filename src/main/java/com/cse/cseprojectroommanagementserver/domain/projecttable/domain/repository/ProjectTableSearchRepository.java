f88870a80889f1478e68942c7b93a975f474c21d
zabcd121 <zabcd121@naver.com>
2022-12-02 10:51:53 +0900

    :fire: delete ProjectTableSearchRepository

--- src/main/java/com/cse/cseprojectroommanagementserver/domain/projecttable/domain/repository/ProjectTableSearchRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cse.cseprojectroommanagementserver.domain.projecttable.domain.repository;
-
-import com.cse.cseprojectroommanagementserver.domain.projecttable.domain.model.ProjectTable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectTableSearchRepository extends JpaRepository<ProjectTable, Long> {
-}
