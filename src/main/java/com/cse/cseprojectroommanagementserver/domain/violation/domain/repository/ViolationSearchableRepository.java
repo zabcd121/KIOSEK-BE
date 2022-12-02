124d1825d0615c9ecb8dabcedc63f65ddb8636a5
zabcd121 <zabcd121@naver.com>
2022-12-02 12:52:42 +0900

    :package: repo(add): Violation Search Repository Interface 정의 및 구현

---
+++ src/main/java/com/cse/cseprojectroommanagementserver/domain/violation/domain/repository/ViolationSearchableRepository.java
@@ -0,0 +1,9 @@
+package com.cse.cseprojectroommanagementserver.domain.violation.domain.repository;
+
+import com.cse.cseprojectroommanagementserver.domain.violation.domain.model.Violation;
+
+import java.util.List;
+
+public interface ViolationSearchableRepository {
+    List<Violation> findNotPenalizedViolationsByMemberId(Long memberId);
+}
