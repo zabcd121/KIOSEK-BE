c13f11235f418c4a09849284597703845af2dc6b
zabcd121 <zabcd121@naver.com>
2022-12-02 10:04:09 +0900

    :necktie: business: Penalty 생성 메서드와 Violation과의 양방향 메서드 추가

--- src/main/java/com/cse/cseprojectroommanagementserver/domain/penalty/domain/model/Penalty.java
+++ src/main/java/com/cse/cseprojectroommanagementserver/domain/penalty/domain/model/Penalty.java
@@ -1,9 +1,12 @@
 package com.cse.cseprojectroommanagementserver.domain.penalty.domain.model;
 
 import com.cse.cseprojectroommanagementserver.domain.member.domain.model.Member;
+import com.cse.cseprojectroommanagementserver.domain.penaltypolicy.domain.model.PenaltyPolicy;
 import com.cse.cseprojectroommanagementserver.domain.violation.domain.model.Violation;
 import com.cse.cseprojectroommanagementserver.global.common.BaseTimeEntity;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
@@ -11,6 +14,8 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Entity
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Penalty extends BaseTimeEntity {
 
@@ -21,11 +26,29 @@ public class Penalty extends BaseTimeEntity {
     private List<Violation> violations;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "MEMBER_ID")
+    @JoinColumn(name = "member_id")
     private Member member;
 
     private String description;
     private LocalDate startDate;
     private LocalDate endDate;
 
+    public void addViolation(Violation violation) {
+        violation.changePenalty(this);
+        this.violations.add(violation);
+    }
+
+    public static Penalty createPenalty(Member member, PenaltyPolicy penaltyPolicy, List<Violation> violations) {
+        Penalty penalty = Penalty.builder()
+                .member(member)
+                .startDate(LocalDate.now())
+                .endDate(LocalDate.now().plusDays(penaltyPolicy.getNumberOfSuspensionDay()))
+                .build();
+
+        for (Violation violation : violations) {
+            penalty.addViolation(violation);
+        }
+
+        return penalty;
+    }
 }
