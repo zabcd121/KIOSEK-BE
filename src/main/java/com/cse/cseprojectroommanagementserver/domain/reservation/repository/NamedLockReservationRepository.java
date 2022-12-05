e55da057d3bb96cedb7a2cddc8640a8a5fb7e957
zabcd121 <zabcd121@naver.com>
2022-12-06 08:10:23 +0900

    :package: Add: Named Lock get and release method

---
+++ src/main/java/com/cse/cseprojectroommanagementserver/domain/reservation/repository/NamedLockReservationRepository.java
@@ -0,0 +1,16 @@
+package com.cse.cseprojectroommanagementserver.domain.reservation.repository;
+
+import com.cse.cseprojectroommanagementserver.domain.reservation.domain.model.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NamedLockReservationRepository extends JpaRepository<Reservation, Long> {
+    @Query(value = "select get_lock(:key, 3000)", nativeQuery = true)
+    void getLock(@Param("key") String key);
+
+    @Query(value = "select release_lock(:key)", nativeQuery = true)
+    void releaseLock(@Param("key") String key);
+}
