35d62bdee5f4af1d6c41f9e34bf15e013074a352
zabcd121 <zabcd121@naver.com>
2022-12-07 22:53:57 +0900

    :package: Add(repo): 개인 멤버별 제재내역 조회 메서드 구현

--- src/main/java/com/cse/cseprojectroommanagementserver/domain/penalty/domain/repository/PenaltySearchableRepository.java
+++ src/main/java/com/cse/cseprojectroommanagementserver/domain/penalty/domain/repository/PenaltySearchableRepository.java
@@ -2,10 +2,13 @@ package com.cse.cseprojectroommanagementserver.domain.penalty.domain.repository;
 
 import com.cse.cseprojectroommanagementserver.domain.penalty.domain.model.Penalty;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PenaltySearchableRepository {
 
     boolean existsByMemberId(Long memberId);
     Optional<Penalty> findInProgressByMemberId(Long memberId);
+
+    Optional<List<Penalty>> findAllByMemberId(Long memberId);
 }
