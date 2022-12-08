05af871880b3c0d4b7482d0ef41f55cca377d05b
zabcd121 <zabcd121@naver.com>
2022-12-09 03:24:42 +0900

    :bomb: Exception: Add 체크인 시에 발생 예외

---
+++ src/main/java/com/cse/cseprojectroommanagementserver/domain/reservation/exception/ImpossibleCheckInTimeAfterStartTimeException.java
@@ -0,0 +1,5 @@
+package com.cse.cseprojectroommanagementserver.domain.reservation.exception;
+
+public class ImpossibleCheckInTimeAfterStartTimeException extends RuntimeException{
+    public ImpossibleCheckInTimeAfterStartTimeException() {}
+}
---
+++ src/main/java/com/cse/cseprojectroommanagementserver/domain/reservation/exception/InvalidReservationQRException.java
@@ -0,0 +1,5 @@
+package com.cse.cseprojectroommanagementserver.domain.reservation.exception;
+
+public class InvalidReservationQRException extends RuntimeException{
+    public InvalidReservationQRException() {}
+}
