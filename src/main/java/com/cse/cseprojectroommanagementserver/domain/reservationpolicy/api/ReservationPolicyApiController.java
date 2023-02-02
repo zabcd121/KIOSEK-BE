d8d3d132ee7154870caa33fbe7f8301ba7eeb5aa
zabcd121 <zabcd121@naver.com>
2023-02-02 23:40:56 +0900

    :sparkles: Feat: 예약 정책 조회 기능 구현

--- src/main/java/com/cse/cseprojectroommanagementserver/domain/reservationpolicy/api/ReservationPolicyApiController.java
+++ src/main/java/com/cse/cseprojectroommanagementserver/domain/reservationpolicy/api/ReservationPolicyApiController.java
@@ -1,15 +1,15 @@
 package com.cse.cseprojectroommanagementserver.domain.reservationpolicy.api;
 
 import com.cse.cseprojectroommanagementserver.domain.reservationpolicy.application.ReservationPolicyChangeService;
+import com.cse.cseprojectroommanagementserver.domain.reservationpolicy.application.ReservationPolicySearchService;
+import com.cse.cseprojectroommanagementserver.global.common.dto.ResponseSuccess;
 import com.cse.cseprojectroommanagementserver.global.common.dto.ResponseSuccessNoResult;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.cse.cseprojectroommanagementserver.domain.reservationpolicy.dto.ReservationPolicyRequestDto.*;
 import static com.cse.cseprojectroommanagementserver.global.common.ResponseConditionCode.RESERVATION_POLICY_CHANGE_SUCCESS;
+import static com.cse.cseprojectroommanagementserver.global.common.ResponseConditionCode.RESERVATION_POLICY_SEARCH_SUCCESS;
 
 @RestController
 @RequestMapping("/api/reservation/policy")
@@ -17,11 +17,17 @@ import static com.cse.cseprojectroommanagementserver.global.common.ResponseCondi
 public class ReservationPolicyApiController {
 
     private final ReservationPolicyChangeService reservationPolicyChangeService;
+    private final ReservationPolicySearchService reservationPolicySearchService;
 
     @PutMapping
     public ResponseSuccessNoResult changeReservationPolicy(@RequestBody ReservationPolicyChangeRequest reservationPolicyChangeRequest) {
         reservationPolicyChangeService.changeReservationPolicy(reservationPolicyChangeRequest);
 
         return new ResponseSuccessNoResult(RESERVATION_POLICY_CHANGE_SUCCESS);
+    }
+
+    @GetMapping
+    public ResponseSuccess getCurrentReservationPolicy() {
+        return new ResponseSuccess(RESERVATION_POLICY_SEARCH_SUCCESS, reservationPolicySearchService.searchReservationPolicy());
     }
 }
