216bf8bbda8d98416094fb6029b03c3a57d572ed
zabcd121 <zabcd121@naver.com>
2023-02-14 16:40:11 +0900

    Update PenaltyApiController.java

--- src/main/java/com/cse/cseprojectroommanagementserver/domain/penalty/api/PenaltyApiController.java
+++ src/main/java/com/cse/cseprojectroommanagementserver/domain/penalty/api/PenaltyApiController.java
@@ -1,9 +1,6 @@
 package com.cse.cseprojectroommanagementserver.domain.penalty.api;
 
 import com.cse.cseprojectroommanagementserver.domain.penalty.application.PenaltySearchService;
-import com.cse.cseprojectroommanagementserver.domain.penalty.domain.repository.PenaltySearchableRepository;
-import com.cse.cseprojectroommanagementserver.domain.penalty.dto.PenaltyResponse;
-import com.cse.cseprojectroommanagementserver.global.common.ResponseConditionCode;
 import com.cse.cseprojectroommanagementserver.global.common.dto.ResponseSuccess;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,7 +21,7 @@ public class PenaltyApiController {
     private final PenaltySearchService penaltySearchService;
 
     @GetMapping
-    public ResponseSuccess<List<PenaltyLogResponse>> getPenaltyLogList(@RequestParam Long memberId) {
+    public ResponseSuccess<List<PenaltyLogResponse>> getPenaltyListByMemberId(@RequestParam Long memberId) {
         List<PenaltyLogResponse> penaltyLogResponseList = penaltySearchService.searchMemberPenaltyLogList(memberId);
         return new ResponseSuccess(PENALTY_LOGS_SEARCH_SUCCESS, penaltyLogResponseList);
     }
