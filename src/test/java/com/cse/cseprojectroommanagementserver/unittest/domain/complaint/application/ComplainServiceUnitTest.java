05b43fd5cc4906f91cef7b7e9703adc926bfae15
zabcd121 <zabcd121@naver.com>
2023-02-27 20:09:54 +0900

    :recycle: Refactor: package 구조 변경 (단위테스트는 unittest 패키지로 옮김)

---
+++ src/test/java/com/cse/cseprojectroommanagementserver/unittest/domain/complaint/application/ComplainServiceUnitTest.java
@@ -0,0 +1,73 @@
+package com.cse.cseprojectroommanagementserver.unittest.domain.complaint.application;
+
+import com.cse.cseprojectroommanagementserver.domain.complaint.application.ComplainService;
+import com.cse.cseprojectroommanagementserver.domain.complaint.domain.model.Complaint;
+import com.cse.cseprojectroommanagementserver.domain.complaint.domain.repository.ComplaintRepository;
+import com.cse.cseprojectroommanagementserver.domain.projectroom.domain.model.ProjectRoom;
+import com.cse.cseprojectroommanagementserver.domain.projectroom.domain.repository.ProjectRoomRepository;
+import com.cse.cseprojectroommanagementserver.domain.tablereturn.exception.ImageUploadFailException;
+import com.cse.cseprojectroommanagementserver.global.common.Image;
+import com.cse.cseprojectroommanagementserver.global.util.FileUploadUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.nio.charset.StandardCharsets;
+
+import static com.cse.cseprojectroommanagementserver.domain.complaint.dto.ComplaintReqDto.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ComplainServiceUnitTest {
+
+    @InjectMocks
+    ComplainService complainService;
+
+    @Mock ComplaintRepository complaintRepository;
+    @Mock ProjectRoomRepository projectRoomRepository;
+    @Mock FileUploadUtil fileUploadUtil;
+
+    /** 테스트 케이스
+     * C1. 민원 접수 성공
+     * C2. 민원 접수 실패
+         * C2-01. 민원 접수 실패 - 이미지 업로드 실패
+     */
+
+    @Test
+    @DisplayName("C1. 민원 접수 성공")
+    void 민원접수_성공() {
+        // Given
+        ComplainReq complainReq = ComplainReq.builder().projectRoomId(1L).subject("subject").content("content").image(new MockMultipartFile("example", "example".getBytes(StandardCharsets.UTF_8))).build();
+        Image uploadedImage = Image.builder().fileLocalName("localName").fileOriName("oriName").fileUrl("/image").build();
+        given(fileUploadUtil.uploadFile(complainReq.getImage())).willReturn(uploadedImage);
+
+        ProjectRoom projectRoom = ProjectRoom.builder().projectRoomId(1L).build();
+        given(projectRoomRepository.getReferenceById(complainReq.getProjectRoomId())).willReturn(projectRoom);
+
+        Complaint savedComplaint = Complaint.builder().complaintId(1L).projectRoom(projectRoom).subject("subject").content("content").image(uploadedImage).build();
+        given(complaintRepository.save(any())).willReturn(savedComplaint);
+
+        // When
+        complainService.complain(complainReq);
+
+        // Then
+        assertDoesNotThrow(() -> ImageUploadFailException.class);
+        then(complaintRepository).should(times(1)).save(any());
+    }
+
+    @Test
+    @DisplayName("C2-01. 민원 접수 실패 - 이미지 업로드 실패")
+    void 민원접수_실패_이미지업로드실패() {
+        // Given
+        ComplainReq complainReq = ComplainReq.builder().projectRoomId(1L).subject("subject").content("content").image(new MockMultipartFile("example", "example".getBytes(StandardCharsets.UTF_8))).build();
+        given(fileUploadUtil.uploadFile(complainReq.getImage())).willThrow(ImageUploadFailException.class);
+
+        // When, Then
+        assertThrows(ImageUploadFailException.class, () -> complainService.complain(complainReq));
+    }
+}
---
+++ src/test/java/com/cse/cseprojectroommanagementserver/unittest/domain/member/application/SignupServiceUnitTest.java
@@ -0,0 +1,128 @@
+package com.cse.cseprojectroommanagementserver.unittest.domain.member.application;
+
+import com.cse.cseprojectroommanagementserver.domain.member.application.SignupService;
+import com.cse.cseprojectroommanagementserver.domain.member.domain.model.Account;
+import com.cse.cseprojectroommanagementserver.domain.member.domain.model.AccountQR;
+import com.cse.cseprojectroommanagementserver.domain.member.domain.model.Member;
+import com.cse.cseprojectroommanagementserver.domain.member.domain.model.RoleType;
+import com.cse.cseprojectroommanagementserver.domain.member.domain.repository.SignupRepository;
+import com.cse.cseprojectroommanagementserver.domain.member.exception.AccountQRNotCreatedException;
+import com.cse.cseprojectroommanagementserver.domain.member.exception.EmailDuplicatedException;
+import com.cse.cseprojectroommanagementserver.domain.member.exception.LoginIdDuplicatedException;
+import com.cse.cseprojectroommanagementserver.global.common.QRImage;
+import com.cse.cseprojectroommanagementserver.global.util.QRGenerator;
+import com.cse.cseprojectroommanagementserver.global.util.QRNotCreatedException;
+import com.google.zxing.WriterException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.io.IOException;
+
+import static com.cse.cseprojectroommanagementserver.domain.member.dto.MemberReqDto.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
+
+@ExtendWith(MockitoExtension.class)
+public class SignupServiceUnitTest {
+    @InjectMocks
+    SignupService signupService;
+
+    @Mock SignupRepository signupRepository;
+    @Spy PasswordEncoder passwordEncoder;
+    @Mock QRGenerator qrGenerator;
+
+    SignupReq signupReq;
+
+    @BeforeEach
+    void setUp() {
+        signupReq = SignupReq.builder()
+                .loginId("20180335")
+                .password("example1234!")
+                .email("example@kumoh.ac.kr")
+                .name("소공이")
+                .build();
+    }
+
+    /** 테스트 케이스
+     * C1. 회원가입 성공
+     * C2. 회원가입 실패
+         * C2-01. 회원가입 실패 - 중복 아이디
+         * C2-02. 회원가입 실패 - 중복 이메일
+         * C2-03. 회원가입 실패 - QR 생성 실패
+     */
+
+    @Test
+    @DisplayName("C1. 회원가입 성공")
+    void 회원가입_성공() throws IOException, WriterException {
+        //Given
+        given(signupRepository.existsByAccountLoginId(signupReq.getLoginId())).willReturn(false);
+        given(signupRepository.existsByEmail(signupReq.getEmail())).willReturn(false);
+
+        QRImage qrImage = QRImage.builder().fileLocalName("localName").fileOriName("account_qr").fileUrl("/Users/khs/Documents/images").content("randomContent").build();
+        given(qrGenerator.createAccountQRCodeImage()).willReturn(qrImage);
+
+        Account account = Account.builder()
+                .loginId(signupReq.getLoginId())
+                .password(signupReq.getPassword())
+                .build();
+        Member savedMember = Member.builder()
+                .memberId(1L)
+                .account(account)
+                .name(signupReq.getName())
+                .email(signupReq.getEmail())
+                .roleType(RoleType.ROLE_MEMBER)
+                .accountQR(AccountQR.builder().qrImage(qrImage).build())
+                .build();
+        given(signupRepository.save(any())).willReturn(savedMember);
+
+        //When
+        signupService.signup(signupReq);
+
+        //Then
+        then(qrGenerator).should(times(1)).createAccountQRCodeImage();
+        then(signupRepository).should(times(1)).save(any());
+    }
+
+    @Test
+    @DisplayName("C2-01. 회원가입 실패 - 중복 아이디")
+    void 회원가입_실패_중복아이디() {
+        // Given
+        given(signupRepository.existsByAccountLoginId(signupReq.getLoginId())).willThrow(LoginIdDuplicatedException.class);
+
+        // When, Then
+        assertThrows(LoginIdDuplicatedException.class, () -> signupService.signup(signupReq));
+    }
+
+    @Test
+    @DisplayName("C2-02. 회원가입 실패 - 중복 이메일")
+    void 회원가입_실패_중복이메일() {
+        // Given
+        given(signupRepository.existsByEmail(signupReq.getEmail())).willThrow(EmailDuplicatedException.class);
+
+        // When, Then
+        assertThrows(EmailDuplicatedException.class, () -> signupService.signup(signupReq));
+    }
+
+    @Test
+    @DisplayName("C2-03. 회원가입 실패 - QR 생성 실패")
+    void 회원가입_실패_QR생성실패() {
+        // Given
+        given(signupRepository.existsByAccountLoginId(signupReq.getLoginId())).willReturn(false);
+        given(signupRepository.existsByEmail(signupReq.getEmail())).willReturn(false);
+
+        given(qrGenerator.createAccountQRCodeImage()).willThrow(QRNotCreatedException.class);
+
+        // When, Then
+        assertThrows(AccountQRNotCreatedException.class, () -> signupService.signup(signupReq));
+    }
+}
