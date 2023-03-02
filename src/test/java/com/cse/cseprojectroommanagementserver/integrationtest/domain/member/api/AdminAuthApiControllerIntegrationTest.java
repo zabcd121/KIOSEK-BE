715990660a5fe97e79c68e5f24373756ad049b27
zabcd121 <zabcd121@naver.com>
2023-03-02 19:11:00 +0900

    :white_check_mark: Test: 관리자 로그인 기능 통합 테스트 코드 작성 (close #126)

---
+++ src/test/java/com/cse/cseprojectroommanagementserver/integrationtest/domain/member/api/AdminAuthApiControllerIntegrationTest.java
@@ -0,0 +1,59 @@
+package com.cse.cseprojectroommanagementserver.integrationtest.domain.member.api;
+
+import com.cse.cseprojectroommanagementserver.domain.member.application.AuthService;
+import com.cse.cseprojectroommanagementserver.domain.member.domain.model.Member;
+import com.cse.cseprojectroommanagementserver.domain.member.dto.MemberReqDto;
+import com.cse.cseprojectroommanagementserver.integrationtest.common.BaseIntegrationTest;
+import com.cse.cseprojectroommanagementserver.integrationtest.setup.MemberSetUp;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class AdminAuthApiControllerIntegrationTest extends BaseIntegrationTest {
+
+    @Autowired
+    private MemberSetUp memberSetUp;
+
+    @Autowired
+    AuthService authService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    /**
+     * M1.관리자 로그인
+         * M1-C01. 관리자 로그인 성공
+     */
+
+    @Test
+    @DisplayName("M1-C01. 로그인 성공")
+    void 관리자로그인_성공() throws Exception {
+        // Given
+        Member savedMember = memberSetUp.saveAdmin("20180335", passwordEncoder.encode("password1!"));
+        MemberReqDto.LoginReq loginReq = MemberReqDto.LoginReq.builder().loginId("20180335").password("password1!").build();
+
+        // When
+        ResultActions resultActions = mvc.perform(
+                        post("/api/admins/v1/login")
+                                .contentType(APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(loginReq))
+                                .characterEncoding("UTF-8")
+                                .accept(APPLICATION_JSON))
+                .andDo(print());
+
+        // Then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.result.memberInfo.memberId").value(savedMember.getMemberId()));
+    }
+
+}
--- src/test/java/com/cse/cseprojectroommanagementserver/integrationtest/setup/MemberSetUp.java
+++ src/test/java/com/cse/cseprojectroommanagementserver/integrationtest/setup/MemberSetUp.java
@@ -2,10 +2,13 @@ package com.cse.cseprojectroommanagementserver.integrationtest.setup;
 
 import com.cse.cseprojectroommanagementserver.domain.member.domain.model.Account;
 import com.cse.cseprojectroommanagementserver.domain.member.domain.model.Member;
+import com.cse.cseprojectroommanagementserver.domain.member.domain.model.RoleType;
 import com.cse.cseprojectroommanagementserver.domain.member.domain.repository.MemberRepository;
 import com.cse.cseprojectroommanagementserver.global.common.QRImage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import static com.cse.cseprojectroommanagementserver.domain.member.domain.model.RoleType.*;
 
 @Component
 public class MemberSetUp {
@@ -20,6 +23,16 @@ public class MemberSetUp {
                         email, name,
                         QRImage.builder().fileOriName(loginId).fileLocalName(loginId).fileUrl("/accounts/2023/03/1").content("!@#ASdasd1!").build()
                 )
+        );
+    }
+
+    public Member saveAdmin(String loginId, String password) {
+        return memberRepository.save(
+                Member.builder()
+                        .account(Account.builder().loginId(loginId).password(password).build())
+                        .roleType(ROLE_ADMIN)
+                        .build()
+
         );
     }
 }
