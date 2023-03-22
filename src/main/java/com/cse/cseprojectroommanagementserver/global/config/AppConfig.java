751dbed32dba60be6a19af12fe0a9ed473608b04
zabcd121 <zabcd121@naver.com>
2023-03-22 17:03:21 +0900

    :wrench: Config: MeterRegistry countedAspect, timedAspect 등록함

--- src/main/java/com/cse/cseprojectroommanagementserver/global/config/AppConfig.java
+++ src/main/java/com/cse/cseprojectroommanagementserver/global/config/AppConfig.java
@@ -2,6 +2,9 @@ package com.cse.cseprojectroommanagementserver.global.config;
 
 import com.cse.cseprojectroommanagementserver.global.config.formatter.LocalDateFormatter;
 import com.cse.cseprojectroommanagementserver.global.config.formatter.LocalDateTimeFormatter;
+import io.micrometer.core.aop.CountedAspect;
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,4 +19,10 @@ public class AppConfig {
     public LocalDateTimeFormatter localDateTimeFormatter() {
         return new LocalDateTimeFormatter();
     }
+
+    @Bean
+    public CountedAspect countedAspect(MeterRegistry meterRegistry) { return new CountedAspect(meterRegistry); }
+
+    @Bean
+    public TimedAspect timedAspect(MeterRegistry meterRegistry) { return new TimedAspect(meterRegistry); }
 }
