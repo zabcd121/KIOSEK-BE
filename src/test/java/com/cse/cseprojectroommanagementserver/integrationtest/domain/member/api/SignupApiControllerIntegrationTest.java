cf27f585225435fec5c1d8a83f5317677fcafa79
zabcd121 <zabcd121@naver.com>
2023-04-27 19:27:02 +0900

    :white_check_mark: Test: kumoh 메일 형식인지 검증 테스트 추가

--- src/test/java/com/cse/cseprojectroommanagementserver/integrationtest/domain/member/api/SignupApiControllerIntegrationTest.java
+++ src/test/java/com/cse/cseprojectroommanagementserver/integrationtest/domain/member/api/SignupApiControllerIntegrationTest.java
@@ -4,9 +4,11 @@ import com.cse.cseprojectroommanagementserver.domain.member.application.EmailSer
 import com.cse.cseprojectroommanagementserver.integrationtest.common.BaseIntegrationTestWithIgnoringURI;
 import com.cse.cseprojectroommanagementserver.integrationtest.setup.MemberSetUp;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -43,6 +45,7 @@ class SignupApiControllerIntegrationTest extends BaseIntegrationTestWithIgnoring
          * M5-C2-01. 회원가입 실패 - ID 중복
          * M5-C2-01. 회원가입 실패 - Email 중복
          * M5-C2-02. 회원가입 실패 - Email 인증코드 미인증 상태
+         * M5-C2-03. 회원가입 실패 - kumoh Email 형식이 아님
      */
 
     @Test
@@ -132,26 +135,6 @@ class SignupApiControllerIntegrationTest extends BaseIntegrationTestWithIgnoring
         resultActions.andExpect(status().isOk());
     }
 
-//    @Test
-//    @DisplayName("M3-C1-01. 인증코드 전송 기능 성공 ")
-//    void 인증코드검증_성공() throws Exception {
-//        // Given
-//        String toEmail = "20180335@kumoh.ac.kr";
-//
-//        // When
-//        ResultActions resultActions = mvc.perform(
-//                        get("/api/v1/members/signup/authcode")
-//                                .param("email", toEmail)
-//                                .characterEncoding("UTF-8")
-//                                .accept(APPLICATION_JSON))
-//                .andDo(print());
-//
-//        // Then
-//        resultActions.andExpect(status().isOk());
-//    }
-
-
-
     @Test
     @DisplayName("M5-C1-01. 회원가입 성공")
     void 회원가입_성공() throws Exception {
@@ -235,6 +218,34 @@ class SignupApiControllerIntegrationTest extends BaseIntegrationTestWithIgnoring
 
         // Then
         resultActions.andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("M5-C2-03. 회원가입 실패 - kumoh Email 형식이 아님")
+    void 회원가입_실패_kumoh메일이아닌경우() throws Exception {
+        // Given
+        String loginIdReq = RandomStringUtils.random(8, false, true);
+        SignupReq signupReq = SignupReq.builder().loginId(loginIdReq).password("password1!").email("email@naver.com").name("김현석").build();
+
+        // When
+        ResultActions resultActions = mvc.perform(
+                        post("/api/v1/members/signup")
+                                .contentType(APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(signupReq))
+                                .characterEncoding("UTF-8")
+                                .accept(APPLICATION_JSON))
+                .andDo(print());
+
+        // Then
+        resultActions.andExpect(status().isBadRequest());
+    }
+
+    @AfterEach
+    void afterEach() {
+        redisTemplate.execute((RedisCallback<Object>) connection -> {
+            connection.flushAll();
+            return null;
+        });
     }
 
 }
