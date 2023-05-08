46adaedef9d2f9301751f02505bcce09a1ec91c1
zabcd121 <zabcd121@naver.com>
2023-05-08 21:00:28 +0900

    :pencil2: Update: 체크인 가능시간을 시작시간 10분전부터를 20분전부터로 변경함
    사용자들 요구사항 반영

--- src/main/java/com/cse/cseprojectroommanagementserver/global/util/ReservationFixedPolicy.java
+++ src/main/java/com/cse/cseprojectroommanagementserver/global/util/ReservationFixedPolicy.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 public enum ReservationFixedPolicy {
-    POSSIBLE_CHECKIN_TIME_BEFORE(10),
+    POSSIBLE_CHECKIN_TIME_BEFORE(20),//2023-05-08 15:00:00 배포에 변경함 (10분 -> 20분)
     POSSIBLE_CHECKIN_TIME_AFTER(20);
 
     private final int value;
