0258ff48ab5dd3f87203a77e6aceca97e708ab63
zabcd121 <zabcd121@naver.com>
2023-05-13 01:27:42 +0900

    Delete ImageUploadFailException.java

--- src/main/java/com/cse/cseprojectroommanagementserver/domain/tablereturn/exception/ImageUploadFailException.java
+++
@@ -1,5 +0,0 @@
-package com.cse.cseprojectroommanagementserver.domain.tablereturn.exception;
-
-public class ImageUploadFailException extends RuntimeException {
-    public ImageUploadFailException() {}
-}
